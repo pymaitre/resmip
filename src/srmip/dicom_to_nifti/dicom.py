673e880c361cfffdb7a98e90fcd7fe356f1450d7
GabrielePalazzo <gabriele.palazzo6.github@gmail.com>
2023-12-13 15:44:15 +0100

    removed commented lines

--- src/srmip/dicom_to_nifti/dicom.py
+++ src/srmip/dicom_to_nifti/dicom.py
@@ -1,18 +1,21 @@
 """Read and write dicom files."""
 
-from typing import Union
 from pathlib import Path
+from typing import Union
+
 import SimpleITK as sitk
 
+
 def read_dicom_series(dicom_series_directory_path: Union[str, Path]) -> None:
-    dicom_series_files = sitk.ImageSeriesReader().GetGDCMSeriesFileNames(str(dicom_series_directory_path))
+    dicom_series_files = sitk.ImageSeriesReader().GetGDCMSeriesFileNames(
+        str(dicom_series_directory_path)
+    )
     slices_number = len(dicom_series_files)
     dicom_series_reader = sitk.ImageSeriesReader()
     dicom_series_reader.SetFileNames(dicom_series_files)
     dicom_series_reader.MetaDataDictionaryArrayUpdateOn()
     dicom_series_reader.LoadPrivateTagsOn()
     dicom_image = dicom_series_reader.Execute()
-    # print(dicom_image)
     slice_0_metadata = {}
     slice_1_metadata = {}
     series_metadata = {}
@@ -23,7 +26,6 @@ def read_dicom_series(dicom_series_directory_path: Union[str, Path]) -> None:
     for k in dicom_series_reader.GetMetaDataKeys(1):
         v = dicom_series_reader.GetMetaData(1, k)
         slice_1_metadata[k] = v
-        # print(k, v)
     for (key, value1), (_, value2) in zip(slice_0_metadata.items(), slice_1_metadata.items()):
         if value1 != value2:
             print(key, value1, value2)
@@ -35,6 +37,7 @@ def read_dicom_series(dicom_series_directory_path: Union[str, Path]) -> None:
             # if the key is not present in the image, we must find a way to keep track of this information
             pass
     import numpy as np
+
     instance_numbers = np.zeros(slices_number, dtype=int)
     image_position_patients = np.zeros((slices_number, 3), dtype=float)
     for i in range(slices_number):
@@ -42,8 +45,13 @@ def read_dicom_series(dicom_series_directory_path: Union[str, Path]) -> None:
         image_position_patient = dicom_series_reader.GetMetaData(i, "0020|0032")
         instance_numbers[i] = instance_number
         image_position_patients[i] = image_position_patient.split("\\")
-    slices_indexes = {"min": instance_numbers.min(), "max": instance_numbers.max()}  # TODO: we must store this information in the image
-    assert slices_indexes["max"] - slices_indexes["min"] + 1 == slices_number  # otherwise, throw an error (missing / duplicated slices)
+    slices_indexes = {
+        "min": instance_numbers.min(),
+        "max": instance_numbers.max(),
+    }  # TODO: we must store this information in the image
+    assert (
+        slices_indexes["max"] - slices_indexes["min"] + 1 == slices_number
+    )  # otherwise, throw an error (missing / duplicated slices)
     # saved as numpy array for convenience, but np.arrays cannot be serialized!
     series_metadata["slice_indexes"] = instance_numbers
     series_metadata["image_positions"] = image_position_patients
@@ -56,12 +64,6 @@ SLICE_DEPENDENT_FIELDS = {
 }
 
 
-study_path = (
-    Path(__file__).parents[3] /
-    "tests" /
-    "Dicom" /
-    "IBSI1_CT_phantom" /
-    "image"
-)
+study_path = Path(__file__).parents[3] / "tests" / "Dicom" / "IBSI1_CT_phantom" / "image"
 print(study_path)
 read_dicom_series(study_path)

3664b1693ad271c3193eb3cbb6073c3947d892f4
GabrielePalazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2024-05-24 16:23:29 +0200

    test: Added test rtst path

--- tests/utils.py
+++ tests/utils.py
@@ -6,3 +6,8 @@ from pathlib import Path
 def dicom_ct_path() -> Path:
     """Path of the test Dicom CT."""
     return Path(__file__).parent / "Dicom" / "IBSI1_CT_phantom" / "CT_00000"
+
+
+def dicom_rtst_path() -> Path:
+    """Path of the test Dicom RTst."""
+    return Path(__file__).parent / "Dicom" / "IBSI1_CT_phantom" / "RTst_00000" / "DCM_RS_00060.dcm"

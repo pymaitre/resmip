0850b8a0c313a01efd01d558363cf004cb4cad9d
Gabriele <73099233+GabrielePalazzo@users.noreply.github.com>
2024-09-28 10:44:55 +0200

    refactor: Using relative imports in __init__.py

--- src/srmip/__init__.py
+++ src/srmip/__init__.py
@@ -2,9 +2,9 @@
 
 import importlib.metadata as im
 
-from srmip.image import Image
-from srmip.rt_structure import RTStructure  # noqa: F401
-from srmip.rt_structure import RTStructureSet  # noqa: F401
+from .image import Image
+from .rt_structure import RTStructure  # noqa: F401
+from .rt_structure import RTStructureSet  # noqa: F401
 
 __version__ = im.version(__package__)
 

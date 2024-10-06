f07c4e12a94402d1a29862e7e0d96d43678fa235
Gabriele <73099233+GabrielePalazzo@users.noreply.github.com>
2024-10-06 15:06:15 +0200

    test: Added structure with different topology (hole)

--- tests/utils.py
+++ tests/utils.py
@@ -13,6 +13,13 @@ def dicom_rtst_path() -> Path:
     return Path(__file__).parent / "Dicom" / "IBSI1_CT_phantom" / "RTst_00000" / "DCM_RS_00060.dcm"
 
 
+def dicom_rtst_path_with_hole() -> Path:
+    """Path of the test Dicom RTst with a hole."""
+    return (
+        Path(__file__).parent / "Dicom" / "IBSI1_CT_phantom" / "RTst_00001" / "DCM_RS_with_hole.dcm"
+    )
+
+
 def ibsi_rtst_path() -> Path:
     """Path of the IBSI-compliant RT Structure mask."""
     return Path(__file__).parent / "Nifti" / "IBSI2_CT_phantom" / "mask" / "GTV-1.nii"

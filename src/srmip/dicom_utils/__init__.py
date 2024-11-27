6fd4cbc4426e5d2a25d87359b0c28e45d92358d1
Gabriele Palazzo <73099233+GabrielePalazzo@users.noreply.github.com>
2024-11-27 10:17:48 +0100

    Added public function for converting slice masks to contours (#35)
    * feat: Added public function for converting slice masks to contours
    
    * feat: Added function for getting the list of polygon contours for the slice mask

--- src/srmip/dicom_utils/__init__.py
+++ src/srmip/dicom_utils/__init__.py
@@ -1,3 +1,7 @@
 """Conversion between DICOM and other ITK formats (e.g.: nifti)."""
 
 from .constants import DICOM_FIELDS  # noqa: F401
+from .rt_utils_wrapper import (  # noqa: F401
+    get_contour_from_slice_mask,
+    get_polygon_contours_from_slice_mask,
+)

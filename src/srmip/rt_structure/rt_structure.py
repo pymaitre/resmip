dfa6845cbf5b55e69c0de1886e62a082cf989895
Gabriele <73099233+GabrielePalazzo@users.noreply.github.com>
2025-07-27 10:20:46 +0200

    feat: Added possibility to create structures directly from numpy arrays

--- src/srmip/rt_structure/rt_structure.py
+++ src/srmip/rt_structure/rt_structure.py
@@ -6,6 +6,7 @@ import logging
 from collections.abc import Iterable
 from pathlib import Path
 
+import numpy as np
 import SimpleITK as sitk
 
 import srmip.dicom_utils.rtst as dicom_rtst
@@ -130,6 +131,48 @@ class RTStructure(Image):
             structure_name = get_structure_name_from_filename(filename)
         new_rt_structure = cls(Image().read_image(filename), name=structure_name)
         return new_rt_structure
+
+    @classmethod
+    def from_array(
+        cls,
+        array: np.ndarray,
+        *,
+        spacing: tuple[float],
+        origin: tuple[float],
+        direction: tuple[float],
+        metadata: dict[str, str] | None = None,
+        name: str = "",
+        **kwargs,
+    ) -> RTStructure:
+        """
+        Create a new structure from a numpy array.
+
+        :param array: 3D array containing voxel values for the structure (z, y, x).
+        :type array: np.ndarray
+        :param spacing: Voxel spacing for the structure in mm (x, y, z).
+        :type spacing: tuple[float]
+        :param origin: Coordinates of the top left voxel in mm (x, y, z).
+        :type origin: tuple[float]
+        :param direction: Direction cosine matrix.
+        :type direction: tuple[float]
+        :param metadata: Metadata containing information from the DICOM header.
+        :type metadata: Dict[str, str]|None
+        :param name: Name of the structure.
+        :type name: str
+        :return: New structure
+        :rtype: RTStructure
+        """
+        return RTStructure(
+            super().from_array(
+                array=array,
+                spacing=spacing,
+                origin=origin,
+                direction=direction,
+                metadata=metadata,
+                **kwargs,
+            ),
+            name=name,
+        )
 
     def write_image(
         self,
